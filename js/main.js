c9b92a09bab1f2ccc560c8b55fee301b7dc04515
Adesh Nilesh Shah <shaha@wt127-01m.ottawa.ad.algonquincollege.com>
2019-11-13 11:45:00 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+if ( 'AmbientLightSensor' in window ) {
+    const sensor = new AmbientLightSensor();
+    sensor.onreading = () => {
+      console.log('illuminance level :', sensor.illuminance);
+        if(sensor.illuminance < 15) {
+              document.querySelector('body').classList.add("dark");
+        }
+        else {
+              document.querySelector('body').classList.remove("dark");
+        }
+    };
+    sensor.onerror = (event) => {
+      console.log(event.error.name, event.error.message);
+    };
+    sensor.start();
+  }
